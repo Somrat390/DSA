df51516780a7a2bbbc2b48757367b59912f01df7
Somrat <somratsheikh426@gmail.com>
2025-05-01 12:08:29 +0600

    Fibonacci memoization using dynamic python

---
+++ DynamicProgramming/fibonacci.py
@@ -0,0 +1,12 @@
+def f(n,dp):
+    if(n <= 1):
+        return n
+    if(dp[n] != -1):
+        return dp[n]
+    
+    dp[n] = f(n-1,dp) + f(n-2,dp)
+    return dp[n]
+
+n = int(input())
+dp = [-1] * (n+1)
+print(f(n,dp))
