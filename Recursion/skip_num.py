06bf4b8dfeae086060e7f15605522b4fe5562871
Somrat <somratsheikh426@gmail.com>
2025-09-09 10:37:59 +0600

    Using only given string

--- Recursion/skip_num.py
+++ Recursion/skip_num.py
@@ -10,3 +10,14 @@ def skip_char(given_string, new_string):
     
 
 print(skip_char("baccad", ""))
+
+def skip_char2(given_string):
+    if len(given_string) == 0:
+        return ""
+    first_char = given_string[0]
+    if first_char == 'a':
+        return skip_char2(given_string[1:])
+    else:
+        return first_char + skip_char2(given_string[1:])
+
+print(skip_char2("baccad"))
